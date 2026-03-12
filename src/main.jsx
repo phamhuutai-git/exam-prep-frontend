febde1c17ba21cfbfd84f194c55322d007dade1d
TruongNgo-05 <ngoquangtruongjk05@gmail.com>
2026-03-12 16:00:30 +0700

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import 'react-toastify/dist/ReactToastify.css';  // thêm dòng này
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from './context/AuthContext.jsx'
-<<<<<<< HEAD
-=======
-
->>>>>>> e69123e557f4b216ae6845ca95fd19a786e37a89
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
